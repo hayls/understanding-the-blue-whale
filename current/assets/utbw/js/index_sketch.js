d22b52a03c8558c149e76fb6bc5be250056886c9
Barton Ip <dorkbydesign@Bartons-MacBook-Pro.local>
2018-10-17 23:40:55 +1100

    Release 0.1.0 - Organised dependencies - Refactored main script - Fixed anchoring issue - Added canvas switching/multiple sketch loading functionality - Improved Sketch01 - Created Sketch2 for the nav box

---
+++ current/assets/utbw/js/index_sketch.js
@@ -0,0 +1,69 @@
+let angle = 0;
+var canvas;
+
+
+function setup() {
+    var cnv = createCanvas(windowWidth, windowHeight, WEBGL);
+
+    cnv.style('z-index', '-99');
+//    cnv.parent("canvas-container");
+    cnv.position(0, 0);
+
+
+    //    canvas = createCanvas (windowWidth, windowHeight, WEBGL);
+    //    canv.position(0,0);
+    //    canv.style('z-index', '99');
+    
+//    pixelDensity(1);
+
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+
+    background('#00334A');
+
+//    loadPixels();
+//
+//    for (var y = 0; y < height; y++) {
+//        for (var x = 0; x < width; x++) {
+//            var index = (x + y * width) * 4;
+//            pixels[index] = x;
+//            pixels[index+1] = 0;
+//            pixels[index+2] = y;
+//            pixels[index+3] = 255;
+//        }
+//
+//    }
+//
+//
+//
+//    updatePixels();
+
+
+
+
+    translate(-1/18 * windowWidth, -1/8 * windowHeight);
+    // rectMode(CENTER);
+    strokeWeight(1.5);
+
+    stroke(255);
+    // noStroke();
+
+    //normalMaterial();
+     fill(255, 30);
+
+
+    rotateX(angle);
+    rotateY(angle * 0.3);
+    rotateZ(angle * 0.2);
+
+    // rect(0, 0, 40, 40);
+
+    box(20);
+
+    angle += 0.06;
+}
---
+++ current/assets/utbw/js/utbw.js
@@ -0,0 +1,161 @@
+$(function () {
+
+
+//    $.scrollify({
+//        section: "section",
+//        scrollbars: false,
+//        before: function (i, panels) {
+//
+//            var ref = panels[i].attr("data-section-name");
+//
+//            $(".pagination .active").removeClass("active");
+//
+//            $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
+//        },
+//        afterRender: function () {
+//            var pagination = "<ul class=\"pagination\">";
+//            var activeClass = "";
+//            $("section").each(function (i) {
+//                activeClass = "";
+//                if (i === 0) {
+//                    activeClass = "active";
+//                }
+//                pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+//            });
+//
+//            pagination += "</ul>";
+//
+//            $(".home").append(pagination);
+//            /*
+//
+//		  Tip: The two click events below are the same:
+//
+//		  $(".pagination a").on("click",function() {
+//			$.scrollify.move($(this).attr("href"));
+//		  });
+//
+//		  */
+//            $(".pagination a").on("click", $.scrollify.move);
+//        }
+//    });
+
+
+    //arc text here
+
+//    const circleType = new CircleType(document.getElementById("arc1"));
+//    circleType.radius(135);
+//
+//
+//    $('#arc1').circleType({
+//        radius: 135
+//    });
+    
+
+
+    //
+    //
+    //    //hover over menu items js
+    //
+    //    document.getElementsByClassName("select").onmouseover = function () {
+    //        mouseOver()
+    //    };
+    //    document.getElementsByClassName("select").onmouseout = function () {
+    //        mouseOut()
+    //    };
+    //
+    //    function mouseOver() {
+    //        document.getElementbyClassName("select").style.color = "red";
+    //    }
+    //
+    //    function mouseOut() {
+    //        document.getElementbyClassName("select").style.color = "white";
+    //    }
+
+
+    $(".select").on('click', function(){
+        let target = $(this).data("option")
+        const options = ['average-size', 'features', 'lifespan', 'comparison']
+
+        if(options.includes(target)){
+            $(`#${target}-text`).show()
+            for(let option of options){
+                if(option != target)
+                    $(`#${option}-text`).hide()
+            }
+        }
+
+    })
+
+    // var cata = document.getElementById("average-size");
+    // cata.onclick = function () {
+    //     document.getElementById("average-size-text").style.display = "flex";
+    //     //        document.getElementById("comparison-text").style.display = "none";
+    //     document.getElementById("features-text").style.display = "none";
+    //     document.getElementById("lifespan-text").style.display = "none";
+    // }
+
+    //    var catb = document.getElementById("comparison");
+    //    catb.onclick = function () {
+    //        document.getElementById("comparison-text").style.display = "flex";
+    //        document.getElementById("average-size-text").style.display = "none";
+    //        document.getElementById("features-text").style.display = "none";
+    //        document.getElementById("lifespan-text").style.display = "none";
+    //    }
+
+
+    // var catc = document.getElementById("features");
+    // catc.onclick = function () {
+        
+    //     console.log(document.getElementById("features-text").style.display)
+        
+    //     document.getElementById("features-text").style.display = "flex";
+    //     document.getElementById("average-size-text").style.display = "none";
+    //     //        document.getElementById("comparison-text").style.display = "none";
+    //     document.getElementById("lifespan-text").style.display = "none";
+    // }
+
+
+    // var catd = document.getElementById("lifespan");
+    // catd.onclick = function () {
+    //     document.getElementById("lifespan-text").style.display = "flex";
+    //     document.getElementById("average-size-text").style.display = "none";
+    //     document.getElementById("features-text").style.display = "none";
+    //     //        document.getElementById("comparison-text").style.display = "none";
+    // }
+
+
+
+    //communication navigation
+
+    var comm1 = document.getElementById("feeding");
+    comm1.onclick = function () {
+        document.getElementById("feeding-text").style.display = "flex";
+        document.getElementById("courting-text").style.display = "none";
+        document.getElementById("ship-text").style.display = "none";
+    }
+
+    var comm2 = document.getElementById("courting");
+    comm2.onclick = function () {
+        document.getElementById("courting-text").style.display = "flex";
+        document.getElementById("feeding-text").style.display = "none";
+        document.getElementById("ship-text").style.display = "none";
+    }
+
+
+    var comm3 = document.getElementById("ship");
+    comm3.onclick = function () {
+        document.getElementById("ship-text").style.display = "flex";
+        document.getElementById("feeding-text").style.display = "none";
+        document.getElementById("courting-text").style.display = "none";
+    }
+
+
+
+
+
+
+
+
+
+    //closing tag
+});
